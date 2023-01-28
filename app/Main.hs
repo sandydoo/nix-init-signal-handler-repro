607fdf08240b0e9f464d7a3c25910fb7c91346e9
sandydoo <hey@sandydoo.me>
2023-01-28 17:50:34 +0400

    Add workaround

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Control.Concurrent (threadDelay)
+import Data.Function ((&))
+import Data.Text (Text)
 import qualified Hercules.CNix as CNix
 import qualified Hercules.CNix.Util as CNix.Util
-import Control.Concurrent (threadDelay)
+import System.Posix.Signals
 
 main :: IO ()
 main = do
+  printBlockedSignals
+
+  putStrLn "Running initNix"
   CNix.init
+
+  printBlockedSignals
+
+  putStrLn "Installing CNix handler"
   CNix.Util.installDefaultSigINTHandler
 
-  print "Initialized Nix store"
+  printBlockedSignals
+
+  -- putStrLn "Unblocking signals hack"
+  -- unblockSignals
+  --   $ emptySignalSet
+  --   & addSignal sigINT
+  --   & addSignal sigTERM
+  --   & addSignal sigHUP
+
+  printBlockedSignals
+
+  putStrLn "Waiting for user interrupt"
 
   -- Wait for 10 seconds
   threadDelay (10 * 1000 * 1000)
 
   print "Exiting"
+
+printBlockedSignals :: IO ()
+printBlockedSignals = do
+  putStrLn "Blocked signals"
+  mask <- getSignalMask 
+  print $ map (isBlocked mask) [sigINT, sigTERM, sigHUP]
+  putStrLn ""
+  where
+    isBlocked :: SignalSet -> Signal -> (Text, Bool)
+    isBlocked mask signal = do
+      (showSignal signal, inSignalSet signal mask)
+
+    showSignal :: Signal -> Text
+    showSignal signal
+      | signal == sigINT = "sigINT"
+      | signal == sigTERM = "sigTERM"
+      | signal == sigHUP = "sigHUP"
+      | otherwise = undefined
+
