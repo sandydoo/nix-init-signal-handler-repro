8af85940c7c561bde119d88045e9db56405bbae5
sandydoo <hey@sandydoo.me>
2023-01-28 23:55:34 +0400

    Debug default handler

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,41 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Main where
 
-import Control.Concurrent (threadDelay)
-import Data.Function ((&))
 import Data.Text (Text)
 import qualified Hercules.CNix as CNix
 import qualified Hercules.CNix.Util as CNix.Util
+import Protolude
 import System.Posix.Signals
+import System.Mem.Weak (deRefWeak)
+import System.Posix (Handler (Catch), installHandler, sigHUP, sigINT, sigTERM, sigUSR1)
 
 main :: IO ()
 main = do
   printBlockedSignals
 
-  putStrLn "Running initNix"
+  putLText "Running initNix"
   CNix.init
 
   printBlockedSignals
 
-  putStrLn "Installing CNix handler"
-  CNix.Util.installDefaultSigINTHandler
+  putLText "Installing CNix handler"
+  installDefaultSigINTHandler
 
   printBlockedSignals
 
-  -- putStrLn "Unblocking signals hack"
-  -- unblockSignals
-  --   $ emptySignalSet
-  --   & addSignal sigINT
-  --   & addSignal sigTERM
-  --   & addSignal sigHUP
+  -- let signalset
+  --       = emptySignalSet
+  --       & addSignal sigINT
+  --       & addSignal sigTERM
+  --       & addSignal sigHUP
+  -- putLText "Unblocking signals hack"
+  -- unblockSignals signalset
 
   printBlockedSignals
 
-  putStrLn "Waiting for user interrupt"
+  putLText "Waiting for user interrupt"
 
   -- Wait for 10 seconds
   threadDelay (10 * 1000 * 1000)
@@ -41,10 +44,10 @@ main = do
 
 printBlockedSignals :: IO ()
 printBlockedSignals = do
-  putStrLn "Blocked signals"
+  putLText "Blocked signals"
   mask <- getSignalMask 
   print $ map (isBlocked mask) [sigINT, sigTERM, sigHUP]
-  putStrLn ""
+  putLText ""
   where
     isBlocked :: SignalSet -> Signal -> (Text, Bool)
     isBlocked mask signal = do
@@ -57,3 +60,37 @@ printBlockedSignals = do
       | signal == sigHUP = "sigHUP"
       | otherwise = undefined
 
+installDefaultSigINTHandler :: IO ()
+installDefaultSigINTHandler = do
+  mainThread <- myThreadId
+  weakId <- mkWeakThreadId mainThread
+  let defaultHaskellHandler = do
+        mt <- deRefWeak weakId
+        for_ mt \t -> do
+          throwTo t (toException UserInterrupt)
+
+  -- Install Nix interrupter in Haskell
+  _oldHandler <-
+    for [sigINT, sigTERM, sigHUP] \sig ->
+      installHandler
+        sig
+        ( Catch do
+            putLText "Interrupt!"
+            CNix.Util.triggerInterrupt
+            defaultHaskellHandler
+        )
+        Nothing
+
+  -- Install dummy SIGUSR1 handler for Nix interrupt signal propagation
+  -- (installHandler uses process-wide sigprocmask, so this should apply to all
+  -- capability threads, as required for Nix)
+  _oldHandler <-
+    installHandler
+      sigUSR1
+      ( -- Not Ignore, because we want to cause EINTR
+        Catch (putLText "sigUSR1" >> pass)
+      )
+      Nothing
+
+  -- Install Haskell interrupter in Nix
+  CNix.Util.createInterruptCallback defaultHaskellHandler
